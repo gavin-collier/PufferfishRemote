fcfa217fbadee1bcddce4dd86ffaf0774c2563c0
Gavin Collier <gavin.mc.collier@gmail.com>
2023-02-20 21:19:47 -0700

    Sever Only

--- README.md
+++
@@ -1,2 +0,0 @@
-# PufferfishRemote
-A project build to allow the usage of smart phone webbrowser's as wii remotes for Dolphan. 
--- client/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.pufferfish {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-    requires socket.io.client;
-    requires engine.io.client;
-    opens com.pufferfish to javafx.fxml;
-    exports com.pufferfish;
-}
